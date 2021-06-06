efb267f4373f66051e0cc169f90022a752052ec9
Jack O'Keeffe <55100016+jackokeeffe@users.noreply.github.com>
2021-06-06 15:01:22 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(
 	name='stock-ticker',
 	version='1.0',
-	packages=['beautifulsoup', 'pyserial'],
+	packages=find_packages(),
 	url='https://jackokeeffe.me/',
 	license='MIT License',
 	author='Jack OKeeffe',
-    description = 'A program to display stock information on LED Matrices.')
+    	description = 'A program to display stock information on LED Matrices',
+	install_requires=['pyserial', 'beautifulsoup4'])
